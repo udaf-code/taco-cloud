0c4452e64176ccc80608e4f2bf236eea197f5341
Виталий <vital@rockey10imac.local>
2026-02-08 08:06:41 +0300

    chapter 2.2

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -5,8 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 @Data
-@AllArgsConstructor
-@NoArgsConstructor(access=AccessLevel.PRIVATE, force=true)
+//@AllArgsConstructor
+//@NoArgsConstructor(access=AccessLevel.PRIVATE, force=true)
 public class Ingredient{
   
   private final String id;
