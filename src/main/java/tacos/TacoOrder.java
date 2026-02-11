f1b38a2a8b7ae56cff9433a50aae01fd1c466d4a
udaf-code <udaf-padonok@mail.ru>
2026-02-11 11:59:34 +0300

    chapter 3.1

--- src/main/java/tacos/TacoOrder.java
+++ src/main/java/tacos/TacoOrder.java
@@ -1,15 +1,26 @@
 package tacos;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+
 import org.hibernate.validator.constraints.CreditCardNumber;
-import java.util.List;
-import java.util.ArrayList;
+
 import lombok.Data;
 
 @Data
-public class TacoOrder {
+public class TacoOrder implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  private Long id;
+
+  private Date placedAt;
+
 
   @NotBlank(message="Delivery name is required")
   private String deliveryName;
@@ -41,4 +52,5 @@ public class TacoOrder {
   public void addTaco(Taco taco) {
     this.tacos.add(taco);
   }
+
 }
