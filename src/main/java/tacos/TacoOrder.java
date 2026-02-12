bda1303011f33a4ebc2238f845fe607981f575ef
Виталий <vital@rockey10imac.local>
2026-02-12 07:49:14 +0300

    ch 3.2

--- src/main/java/tacos/TacoOrder.java
+++ src/main/java/tacos/TacoOrder.java
@@ -9,18 +9,21 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.CreditCardNumber;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 import lombok.Data;
 
 @Data
+@Table
 public class TacoOrder implements Serializable {
 
   private static final long serialVersionUID = 1L;
 
+  @Id
   private Long id;
 
-  private Date placedAt;
-
+  private Date placedAt = new Date();
 
   @NotBlank(message="Delivery name is required")
   private String deliveryName;
--- src/main/java/tacos/data/IngredientRepository.java
+++ src/main/java/tacos/data/IngredientRepository.java
@@ -1,10 +1,11 @@
 package tacos.data;
 
 import java.util.Optional;
-
+import org.springframework.data.repository.CrudRepository;
 import tacos.Ingredient;
 
-public interface IngredientRepository {
+public interface IngredientRepository 
+extends CrudRepository<Ingredient, String> {
 
   Iterable<Ingredient> findAll();
   
