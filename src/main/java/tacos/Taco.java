8039c1de056e9a12033839ac278f39a7f56c3abc
Виталий <vital@rockey10imac.local>
2026-02-13 07:12:59 +0300

    ch 3.2 end

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -6,11 +6,22 @@ import java.util.List;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@Table
+// Exclude createdAt from equals() method so that tests won't fail trying to
+// compare java.util.Date with java.sql.Timestamp (even though they're essentially
+// equal). Need to figure out a better way than this, but excluding this property
+// for now.
+@EqualsAndHashCode(exclude = "createdAt")
 public class Taco {
 
+  @Id
   private Long id;
 
   private Date createdAt = new Date();
