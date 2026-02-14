c79e2aacf5c42a7ae45ccb447d9ab02fce8faf9b
Vitaliy <udaf-@rockey8.(none)>
2026-02-14 07:29:18 +0300

    ch 4.1.1

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -1,20 +1,21 @@
 package tacos;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+import org.springframework.data.cassandra.core.mapping.Table;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Data
-@Entity
 @AllArgsConstructor
 @NoArgsConstructor(access=AccessLevel.PRIVATE, force=true)
+@Table("ingredients")
 public class Ingredient {
 
-  @Id
+  @PrimaryKey
   private String id;
   private String name;
   private Type type;
