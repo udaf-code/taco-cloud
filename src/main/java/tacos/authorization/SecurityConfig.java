64646d0347a8710942e7c8a3de583d2f8b8612fb
Виталий <vital@rockey10imac.local>
2026-03-08 08:27:31 +0300

    error 404

--- src/main/java/tacos/authorization/SecurityConfig.java
+++ src/main/java/tacos/authorization/SecurityConfig.java
@@ -5,35 +5,41 @@ import org.springframework.security.config.annotation.web.builders.
 import org.springframework.security.config.annotation.web.configuration.
               EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import tacos.authorization.users.UserRepository;
+import tacos.authorization.users.User;
 
 @EnableWebSecurity
 public class SecurityConfig {
 
-	@Bean
-	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
-	        throws Exception {
-		return http
-			.authorizeRequests(authorizeRequests ->
-				authorizeRequests.anyRequest().authenticated()
-			)
+ @Bean
+ SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+  return http
+   .authorizeRequests(authorizeRequests ->
+    authorizeRequests.anyRequest().authenticated()
+   )
+   .formLogin()
+   .and()
+   .build();
+ }
 
-			.formLogin()
+ @Bean
+ public PasswordEncoder passwordEncoder() {
+  return new BCryptPasswordEncoder();
+ }
 
-			.and().build();
-	}
-
-	@Bean
-	UserDetailsService userDetailsService(UserRepository userRepo) {
-	  return username -> userRepo.findByUsername(username);
-	}
-
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-	  return new BCryptPasswordEncoder();
-	}
+ @Bean
+ public UserDetailsService userDetailsService(UserRepository userRepo) {
+  return username -> {
+   User user = userRepo.findByUsername(username);
+   if (user != null) {
+    return user;
+   }
+   throw new UsernameNotFoundException("User '" + username + "' not found");
+  };
+ }
 }
